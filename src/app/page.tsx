1a5b4e08693917513cd54e27febd7a73a8ea66a7
bakuga <maximgancharik@mail.ru>
2025-08-07 20:08:44 +0300

    fix// Tailwind + css переменные

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import '@/styles/_colors.css'
+
 
 export default function Home() {
   return (
     <>
       <div className='text-4xl font-bold text-[var(--color-accent-100)]'>Tailwind работает! 🚀</div>
-      <div className='text-4xl font-bold text-accent-100'>Tailwind работает! 🚀</div>
+      <div className='text-accent-100 text-4xl font-bold'>Tailwind работает! 🚀</div>
       <div className='bg-danger-100 p-4 text-white'>Test</div>
       <div style={{ width: '50px', height: '50px', color: 'var(--color-accent-100)' }}>123123123213</div>
+      <div className="text-accent-500 text-4xl font-bold">Тест цвета Accent</div>
+      <div className="bg-danger-300 p-4 text-white">Тест фона Danger</div>
     </>
   )
 }
