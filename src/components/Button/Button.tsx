b1d7a63c7ad193696679aef15cb16769af06411f
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-08 18:41:47 +0300

    refactor(Button): improve ButtonProps type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,33 +1,30 @@
 'use client'
-
 import React from 'react'
-
-import styles from './Button.module.css'
+import { VariantUnitType, SizeUnitType } from '@/common/types'
+import s from './Button.module.css'
 
 type ButtonProps = Partial<{
-  children: React.ReactNode
   onClick: () => void
-  variant: 'primary' | 'secondary' | 'transparent' | 'default'
-  size: 'small' | 'medium' | 'large'
-  disabled: boolean
+  variant: VariantUnitType
+  size: SizeUnitType
   loading: boolean
-}>
+} & React.ButtonHTMLAttributes<HTMLSelectElement>>
 
 export const Button: React.FC<ButtonProps> = ({ children, onClick, variant, size, disabled, loading }: ButtonProps) => {
   const variantClass =
     variant === 'secondary'
-      ? styles.buttonSecondary
+      ? s.buttonSecondary
       : variant === 'transparent'
-        ? styles.buttonTransparent
+        ? s.buttonTransparent
         : variant === 'primary'
-          ? styles.buttonPrimary
-          : styles.buttonDefault
-  const sizeClass = size === 'small' ? styles.buttonSmall : size === 'large' ? styles.buttonLarge : styles.buttonMedium
-  const disabledClass = disabled || loading ? styles.buttonDisabled : ''
+          ? s.buttonPrimary
+          : s.buttonDefault
+  const sizeClass = size === 'small' ? s.buttonSmall : size === 'large' ? s.buttonLarge : s.buttonMedium
+  const disabledClass = disabled || loading ? s.buttonDisabled : ''
 
   return (
     <button
-      className={`${styles.button} ${variantClass} ${sizeClass} ${disabledClass}`}
+      className={`${s.button} ${variantClass} ${sizeClass} ${disabledClass}`}
       onClick={onClick}
       disabled={disabled || loading}
     >
