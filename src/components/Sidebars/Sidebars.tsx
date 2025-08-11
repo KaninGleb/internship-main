ac5089bbd021bef334cda44a19d6cf17eab17be6
Grensel <sergeiodinochenko@mail.ru>
2025-08-12 00:29:48 +0300

    I20-40 feat: added all links

--- src/components/Sidebars/Sidebars.tsx
+++ src/components/Sidebars/Sidebars.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import { Icon } from "../Icon/Icon"
+import Link from "next/link"
 
 export const Sidebar = () => {
-  const [activeId, setActiveId] = useState<string>("home")
+  const [activeItem, setActiveItem] = useState<string>("home")
 
   const primaryLinks = [
     { href: "/", label: "Feed", icon: "home-outline", activeIcon: "home" },
@@ -10,7 +11,6 @@ export const Sidebar = () => {
     { href: "/profile", label: "My Profile", icon: "person-outline", activeIcon: "person" },
     { href: "/messenger", label: "Messenger", icon: "message-circle-outline", activeIcon: "message-circle" },
     { href: "/search", label: "Search", icon: "search-outline", activeIcon: "search" },
-    { href: "/logout", label: "Logout", icon: "log-out-outline", activeIcon: "log-out" },
   ]
 
   const secondaryLinks = [
@@ -19,30 +19,65 @@ export const Sidebar = () => {
   ]
 
   return (
-    <aside className="mt-[60px] w-[220px] pl-[60px] pt-[72px] text-light-100 line-">
+    <aside className="w-[220px] h-screen mt-[60px] pl-[60px] pt-[72px] border-r border-dark-300 text-light-100 font-medium font-medium_text-14 text-sm leading-6"> 
       <nav>
-        <ul>
+        <ul className="mb-[60px] flex flex-col gap-6">
           {primaryLinks.map(({ href, label, icon, activeIcon }) => {
-            const isActive = href === activeId
+            const isActive = href === activeItem
             return (
               <li key={href}>
-                <a className={`flex cursor-pointer items-center ${isActive ? 'text-info-500' : ''}`}
+                <Link className={`flex cursor-pointer items-center ${isActive ? 'text-info-500' : ''}`}
                   href={href}
                   onClick={(e) => {
                     e.preventDefault()
-                    setActiveId(href)
+                    setActiveItem(href)
                   }}>
                   <Icon
                     className="fill-current mr-3"
                     iconId={isActive ? activeIcon : icon}
                     size={24}
                   />
-                  {label}
-                </a>
+                  <span>{label}</span>
+                </Link>
               </li>
             )
           })}
         </ul>
+        <ul className="mb-[180px] flex flex-col gap-6">
+          {secondaryLinks.map(({ href, label, icon, activeIcon }) => {
+            const isActive = href === activeItem
+            return (
+              <li key={href}>
+                <Link className={`flex cursor-pointer items-center ${isActive ? 'text-info-500' : ''}`}
+                  href={href}
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setActiveItem(href)
+                  }}>
+                  <Icon
+                    className="fill-current mr-3"
+                    iconId={isActive ? activeIcon : icon}
+                    size={24}
+                  />
+                  <span>{label}</span>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+        <Link className={`flex cursor-pointer items-center ${activeItem === '/logout' ? 'text-info-500' : ''}`}
+          href={"/logout"}
+          onClick={(e) => {
+            e.preventDefault()
+            setActiveItem("/logout")
+          }}>
+          <Icon
+            className="fill-current mr-3"
+            iconId={activeItem === "/logout" ? "log-out" : "log-out-outline"}
+            size={24}
+          />
+          <span>Logout</span>
+        </Link>
       </nav>
     </aside>
   )
