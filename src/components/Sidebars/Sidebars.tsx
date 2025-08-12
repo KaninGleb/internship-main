81b1e1d5c6f83ce7ca6f4eb7d8e47378a73db91d
Grensel <sergeiodinochenko@mail.ru>
2025-08-12 18:54:05 +0300

    feat: added focus active hover

--- src/components/Sidebars/Sidebars.tsx
+++ src/components/Sidebars/Sidebars.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import { Icon } from "../Icon/Icon"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export const Sidebar = () => {
-  const [activeItem, setActiveItem] = useState<string>("home")
+  const pathname = usePathname()
+  const [activeItem, setActiveItem] = useState<string | null>(pathname)
 
   const primaryLinks = [
     { href: "/", label: "Feed", icon: "home-outline", activeIcon: "home" },
@@ -19,19 +21,22 @@ export const Sidebar = () => {
   ]
 
   return (
-    <aside className="w-[220px] h-screen mt-[60px] pl-[60px] pt-[72px] border-r border-dark-300 text-light-100 font-medium font-medium_text-14 text-sm leading-6"> 
-      <nav>
-        <ul className="mb-[60px] flex flex-col gap-6">
+    <aside className="w-[220px] h-screen mt-[60px] pl-[60px] pt-[72px] align-middle border-r border-dark-300 text-light-100 font-medium font-medium_text-14 text-sm leading-6">
+      <nav className="outline-none">
+        <ul className="mb-[56px] flex flex-col items-start gap-5 ">
           {primaryLinks.map(({ href, label, icon, activeIcon }) => {
             const isActive = href === activeItem
             return (
               <li key={href}>
-                <Link className={`flex cursor-pointer items-center ${isActive ? 'text-info-500' : ''}`}
+                <Link className={`flex cursor-pointer items-center pr-2 ${isActive ? 'text-info-500' : ''} hover:text-info-100
+                border-2 border-transparent rounded-[2px] focus:outline-none focus:ring-0 focus:border-info-700 disabled:test-dark-100
+                transition-colors ease-in-out duration-200`}
                   href={href}
                   onClick={(e) => {
                     e.preventDefault()
                     setActiveItem(href)
-                  }}>
+                  }}
+                  aria-disabled={"false"}>
                   <Icon
                     className="fill-current mr-3"
                     iconId={isActive ? activeIcon : icon}
@@ -43,12 +48,14 @@ export const Sidebar = () => {
             )
           })}
         </ul>
-        <ul className="mb-[180px] flex flex-col gap-6">
+        <ul className="mb-[176px] flex flex-col items-start gap-5">
           {secondaryLinks.map(({ href, label, icon, activeIcon }) => {
             const isActive = href === activeItem
             return (
               <li key={href}>
-                <Link className={`flex cursor-pointer items-center ${isActive ? 'text-info-500' : ''}`}
+                <Link className={`flex cursor-pointer items-center ${isActive ? 'text-info-500' : ''} hover:text-info-100
+                border-2 border-transparent rounded-[2px] focus:outline-none focus:ring-0 focus:border-info-700 disabled:test-dark-100
+                transition-colors ease-in-out duration-200`}
                   href={href}
                   onClick={(e) => {
                     e.preventDefault()
@@ -65,8 +72,10 @@ export const Sidebar = () => {
             )
           })}
         </ul>
-        <Link className={`flex cursor-pointer items-center ${activeItem === '/logout' ? 'text-info-500' : ''}`}
-          href={"/logout"}
+        <Link className={`inline-flex cursor-pointer ${activeItem === '/logout' ? 'text-info-500' : ''} hover:text-info-100
+                border-2 border-transparent rounded-[2px] focus:outline-none focus:ring-0 focus:border-info-700 disabled:test-dark-100
+                transition-colors ease-in-out duration-200`}
+          href={"/sign-in"}
           onClick={(e) => {
             e.preventDefault()
             setActiveItem("/logout")
