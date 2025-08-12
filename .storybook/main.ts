4523001cb8fef30b4d24a51fa62491ebe205af3b
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-12 22:34:57 +0300

    feat(ci): add chromatic for visual testing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@chromatic-com/storybook', '@storybook/addon-docs', '@storybook/addon-onboarding', '@storybook/addon-a11y'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-onboarding', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
