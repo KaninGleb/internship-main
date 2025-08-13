c8ae627747036ef74baf9f6ba912d728d7cc475c
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-13 21:50:04 +0300

    feat(pagination): Add pagination feature

---
+++ src/components/Pagination/Select/Select.tsx
@@ -0,0 +1,48 @@
+import React, { ChangeEvent } from 'react'
+
+
+type OptionType = {
+  id: string | number
+  value: string | number
+}
+
+type SelectPropsType = {
+  options?: OptionType[]
+  className?: string
+  onChangeOption?: (option: number) => void
+  value: number
+}
+
+export const Select: React.FC<SelectPropsType> = ({
+  options,
+  className,
+  onChangeOption,
+  value,
+}) => {
+  const mappedOptions: React.ReactNode[] = options
+    ? options.map((o) => (
+        <option id={'hw7-option-' + o.id} key={o.id} value={o.value}>
+          {o.value}
+        </option>
+      ))
+    : []
+
+  const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (onChangeOption) {
+      onChangeOption(+e.currentTarget.value)
+    }
+
+  }
+
+  return (
+    <select
+            onChange={onChangeCallback}
+            className={className}
+            value={value}
+        >
+      {mappedOptions}
+    </select>
+  )
+}
+
+
