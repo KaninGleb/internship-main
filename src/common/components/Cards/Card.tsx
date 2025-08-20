490083861aa311450666ba97bf6b3ddbbf49a7bd
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-20 21:20:43 +0300

    refactor: Improve Card component and use in SignUpPage

--- src/common/components/Cards/Card.tsx
+++ src/common/components/Cards/Card.tsx
@@ -1,20 +1,11 @@
-import React, { ComponentProps, forwardRef } from 'react'
+import { ComponentProps, forwardRef } from 'react'
 import { cn } from '@/common/utils'
 
-type CardProps = {
-  className?: string
-} & ComponentProps<'div'>
+type CardProps = ComponentProps<'div'>
 
 export const Card = forwardRef<HTMLDivElement, CardProps>(({ children, className, ...props }, ref) => {
   return (
-    <div
-      className={cn(
-        'min-h-[100px] min-w-[100px] rounded-xs border border-[var(--color-dark-300)] bg-[var(--color-dark-500)]',
-        className,
-      )}
-      ref={ref}
-      {...props}
-    >
+    <div ref={ref} className={cn('border-dark-300 bg-dark-500 rounded-xs border', className)} {...props}>
       {children}
     </div>
   )
