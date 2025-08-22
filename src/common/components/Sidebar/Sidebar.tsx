40b7ae60480824718078dd8af77294826eb905c1
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-22 16:27:28 +0300

    refactor(Sidebar): optimize component logic

--- src/common/components/Sidebar/Sidebar.tsx
+++ src/common/components/Sidebar/Sidebar.tsx
@@ -1,21 +1,13 @@
 'use client'
 
 import { Icon } from '@/common/components'
-import { IconIdType } from '@/common/types'
 import { usePathname, useRouter } from 'next/navigation'
-import { SidebarLink } from './SidebarLink/SidebarLink'
-
-export type LinksType = {
-  href: string
-  label: string
-  iconId: IconIdType
-  activeIconId: IconIdType
-  disabled: boolean
-}
+import { SidebarLinks } from './SidebarLinks/SidebarLinks'
+import { sidebarLinks } from './Sidebar.config'
+import { cn } from '@/common/utils'
 
 export const Sidebar = () => {
   const pathname = usePathname()
-
   const router = useRouter()
 
   const handleLogout = () => {
@@ -23,48 +15,39 @@ export const Sidebar = () => {
   }
 
   const isLoggedIn = true
+  const isAdmin = false
 
-  // Если пользователь не авторизован — не отображаем сайдбар
+  const linkGroup = isAdmin ? sidebarLinks.admin : sidebarLinks.user
+
   if (!isLoggedIn) return null
 
-  const primaryLinks: LinksType[] = [
-    { href: '/', label: 'Feed', iconId: 'home-outline', activeIconId: 'home', disabled: false },
-    { href: '/create', label: 'Create', iconId: 'plus-square-outline', activeIconId: 'plus-square', disabled: false },
-    { href: '/profile', label: 'My Profile', iconId: 'person-outline', activeIconId: 'person', disabled: false },
-    {
-      href: '/messenger',
-      label: 'Messenger',
-      iconId: 'message-circle-outline',
-      activeIconId: 'message-circle',
-      disabled: false,
-    },
-    { href: '/search', label: 'Search', iconId: 'search', activeIconId: 'search', disabled: false },
-  ]
-
-  const secondaryLinks: LinksType[] = [
-    { href: '/statistics', label: 'Statistics', iconId: 'trending-up', activeIconId: 'trending-up', disabled: false },
-    { href: '/favorities', label: 'Favorites', iconId: 'bookmark-outline', activeIconId: 'bookmark', disabled: false },
-  ]
-
   return (
-    <aside className='bg-dark-700 border-dark-300 text-light-100 fixed left-0 h-[100vh] w-[220px] border-r pt-[70px] pl-[58px] align-middle text-sm leading-6 font-bold'>
-      <nav>
-        <SidebarLink links={primaryLinks} pathname={pathname} className={'mb-[56px] flex flex-col items-start gap-5'} />
-        <SidebarLink
-          links={secondaryLinks}
-          pathname={pathname}
-          className={'mb-[176px] flex flex-col items-start gap-5'}
-        />
+    <aside
+      className={cn(
+        'bg-dark-700 border-dark-300 text-light-100 fixed bottom-0 left-0 h-full w-[220px] border-r pt-32 pb-9',
+        'pl-[58px] align-middle text-sm leading-6 font-bold',
+      )}
+    >
+      <nav className={'flex h-full flex-col justify-between'}>
+        <div>
+          {linkGroup.map((g, index) => (
+            <SidebarLinks
+              key={index}
+              links={g.items}
+              pathname={pathname}
+              className={linkGroup.length > 1 && index < linkGroup.length - 1 ? 'mb-14' : ''}
+            />
+          ))}
+        </div>
+
         <button
-          className={`inline-flex cursor-pointer pr-2 ${pathname === '/sign-in' && 'text-accent-500'} hover:text-accent-100 focus:border-accent-700 disabled:text-dark-100 rounded-[2px] border-2 border-transparent transition-colors duration-200 ease-in-out focus:ring-0 focus:outline-none`}
+          className={`inline-flex w-fit cursor-pointer pr-2 ${pathname === '/sign-in' && 'text-accent-500'} hover:text-accent-100 focus:border-accent-700 disabled:text-dark-100 rounded-[2px] border-2 border-transparent transition-colors duration-200 ease-in-out focus:ring-0 focus:outline-none`}
           onClick={handleLogout}
         >
-          <Icon className='mr-3 fill-current' iconId={'log-out'} size={24} />
+          <Icon className='mr-3 fill-current' iconId={'log-out'} />
           <span>Log Out</span>
         </button>
       </nav>
     </aside>
   )
 }
-
-export default Sidebar
