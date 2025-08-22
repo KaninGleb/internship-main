dd2baf74c7fa23d28b67941b53334308a17740a2
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-22 20:31:52 +0300

    refactor(components): Replace PageWrapper with polymorphic FlexWrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,9 @@ import { SignUpPage } from '@/pages/SignUpPage/SignUpPage'
 
 export default function Home() {
   return (
-    <>
+    <div className={'flex h-screen flex-col'}>
       <Header isAuth={false} isAdmin={false} />
-
       <SignUpPage />
-    </>
+    </div>
   )
 }
