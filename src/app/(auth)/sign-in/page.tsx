aa8dc83e10f830d1fb3ffcae15160b65227448b7
Grensel <sergeiodinochenko@mail.ru>
2025-08-22 22:30:15 +0300

    feat: add styles to component

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,19 +4,25 @@ import { Card, Header, Icon, Typography } from '@/common/components'
 import { SignInForm } from '@/features/auth/Index'
 
 const SignInPage = () => (
-  <div className='flex w-screen flex-col items-center'>
+  <>
     <Header isAuth={false} isAdmin={false} />
-    <Card className={'flex flex-col items-center'}>
-      <Typography as={'h1'} variant={'h1'}>
-        Sign In
-      </Typography>
-      <div className={'flex'}>
-        <Icon iconId='google' size={36} />
-        <Icon iconId='github' size={36} color='var(--color-light-100)' />
-      </div>
-      <SignInForm />
-    </Card>
-  </div>
+    <main className='flex h-screen flex-col items-center'>
+      <Card className={'flex w-95 flex-col items-center p-6'}>
+        <Typography as={'h1'} variant={'h1'}>
+          Sign In
+        </Typography>
+        <div className={'mt-3.5 mb-6 flex gap-15'}>
+          <a href={'https://www.google.com/'}>
+            <Icon iconId='google' size={36} className='transition hover:scale-110' />
+          </a>
+          <a href={'https://github.com/'}>
+            <Icon iconId='github' size={36} color='var(--color-light-100)' className='transition hover:scale-110' />
+          </a>
+        </div>
+        <SignInForm />
+      </Card>
+    </main>
+  </>
 )
 
 export default SignInPage
--- src/features/auth/singIn/SignInForm.tsx
+++ src/features/auth/singIn/SignInForm.tsx
@@ -5,17 +5,23 @@ import Link from 'next/link'
 
 export const SignInForm = () => {
   return (
-    <form action=''>
-      <div>
-        <Input type={'email'} placeholder={'Epam@epam.com'} label={'Email'} />
-        <Input type={'password'} placeholder={'**********'} />
+    <form className='flex w-full flex-col justify-center'>
+      <div className='mb-5 flex flex-col gap-8'>
+        <Input className='w-full' type={'email'} placeholder={'Epam@epam.com'} label={'Email'} />
+        <Input className='w-full' type={'password'} placeholder={'**********'} />
       </div>
-      <Link href={'/forgot-password'}>Forgot Password</Link>
-      <Button type={'submit'}>Sign In</Button>
-      <Typography>Don’t have an account?</Typography>
-      <Button variant='default'>
-        <Link href={'/sign-up'}>Sign Up</Link>
+      <Link href={'/forgot-password'} className='text-light-900 mb-6 self-end px-1 text-sm leading-6 font-normal'>
+        Forgot Password{' '}
+      </Link>
+      <Button type={'submit'} variant='primary'>
+        Sign In
       </Button>
+      <div className='mt-4.5 flex w-full flex-col items-center'>
+        <Typography className='mb-1.5 text-base leading-6 font-normal'>Don’t have an account?</Typography>
+        <Button variant='default'>
+          <Link href={'/'}>Sign Up</Link>
+        </Button>
+      </div>
     </form>
   )
 }
