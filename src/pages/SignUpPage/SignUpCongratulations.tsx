ebe87688acd36baba8b5a6e4c0710cc9d8b8c0e0
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-23 18:54:57 +0300

    refactor(SignUpCongratulations): Replace component JSX with FeedbackLayout

--- src/pages/SignUpPage/SignUpCongratulations.tsx
+++ src/pages/SignUpPage/SignUpCongratulations.tsx
@@ -1,42 +1,21 @@
-import { Button, Header } from '@/common/components'
-import { cn } from '@/common/utils'
-import Image from 'next/image'
+import Link from 'next/link'
+import { Button, FeedbackLayout } from '@/common/components'
 import { gitlByTheWall } from '@/assets/images'
 
-//
-
 export const SignUpCongratulations = () => {
-  const commonClasses = 'font-[var(--font-family-primary)] text-center'
-
   return (
-    <div className={'overflow-hidden'}>
-      <Header isAuth notifCounter={0} />
+    <FeedbackLayout.Root>
+      <FeedbackLayout.Title>Congratulations!</FeedbackLayout.Title>
 
-      <div className='bg-dark-700 flex min-h-screen flex-col items-center pt-8.75 text-white'>
-        <h1
-          className={cn(
-            commonClasses,
-            'pb-5 text-[20px] leading-[var(--line-height-l)] font-[var(--font-weight-bold)]',
-          )}
-        >
-          Congratulations!
-        </h1>
-        <p
-          className={cn(
-            commonClasses,
-            'pb-14 text-[16px] leading-[var(--line-height-m)] font-[var(--font-weight-regular)]',
-          )}
-        >
-          Your email has been confirmed.
-        </p>
-        <Button variant={'primary'} className={'w-[182px]'} children={'Sign In'} />
+      <FeedbackLayout.Message className='pb-14'>Your email has been confirmed.</FeedbackLayout.Message>
 
-        <div className='relative mt-20 flex justify-center'>
-          <Image src={gitlByTheWall} alt='Girl by the wall' className='object-contain' unoptimized priority={true} />
-        </div>
-      </div>
-    </div>
+      <Link href={'/sign-in'}>
+        <Button variant={'primary'} className={'mb-[72px] w-[182px]'}>
+          Sign In
+        </Button>
+      </Link>
+
+      <FeedbackLayout.Image src={gitlByTheWall} alt='Girl by the wall' />
+    </FeedbackLayout.Root>
   )
 }
-
-export default SignUpCongratulations
